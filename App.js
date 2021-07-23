c256c732056993d6573aa3fe3435df07b40523ae
ryuken73 <ryuken73@gmail.com>
2021-07-23 21:30:35 +0900

    update: initial working version

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      source={{uri: 'http://cctvmap.sbs.co.kr/map'}}
+      style={{marginTop: 10, marginBottom: 10}}
+    >
+    </WebView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
